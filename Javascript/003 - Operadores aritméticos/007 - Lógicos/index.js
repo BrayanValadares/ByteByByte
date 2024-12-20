105dcb4adff7e01c5bd9d1e0fe87f16ba7b8225b
Brayan <brayanvaladaresp@gmail.com>
2024-12-20 20:30:43 -0300

    build: Starting with Javascript 2

---
+++ Javascript/003 - Operadores aritméticos/007 - Lógicos/index.js
@@ -0,0 +1,17 @@
+console.log("***** E (&&) *****");
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+
+console.log("***** OU (||) *****");
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+
+console.log("***** Negação (!) *****");
+console.log(!true); // false
+console.log(!(1 + 1 === 3)) // true
