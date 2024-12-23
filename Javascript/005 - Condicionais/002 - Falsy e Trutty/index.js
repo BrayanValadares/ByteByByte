5bdaa553cd4a51873906e932a4f4957a4638e073
Brayan <brayanvaladaresp@gmail.com>
2024-12-23 11:47:20 -0300

    feat: add JavaScript variable and conditional exercises with examples

---
+++ Javascript/005 - Condicionais/002 - Falsy e Trutty/index.js
@@ -0,0 +1,13 @@
+// Falsy = false, 0, "", NaN, null, undefined
+// Trutty = Todos os outros diferentes de [Falsy]
+
+
+if(null){
+  console.log("bom dia")
+} else{
+  console.log("boa tarde") 
+}
+
+if(!null){
+  console.log("Falsy or False");
+}
